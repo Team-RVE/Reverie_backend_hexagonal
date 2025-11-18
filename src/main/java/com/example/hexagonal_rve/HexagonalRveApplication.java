cd19011110b3b372152fdcde10c4fb090046c674
yellowstarr0323 <ihansaem60@gmail.com>
2025-11-18 19:10:46 +0900

    init project

---
+++ src/main/java/com/example/hexagonal_rve/HexagonalRveApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal_rve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalRveApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HexagonalRveApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/hexagonal_rve/HexagonalRveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal_rve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalRveApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
