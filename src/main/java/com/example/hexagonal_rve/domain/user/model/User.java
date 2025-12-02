68dca60f7f5e95976157e34ba4d4926df05405e2
yellowstarr0323 <ihansaem60@gmail.com>
2025-12-02 22:51:11 +0900

    User 도메인 빌더 어노테이션 추가 및 restore메서드 생성

--- src/main/java/com/example/hexagonal_rve/domain/user/model/User.java
+++ src/main/java/com/example/hexagonal_rve/domain/user/model/User.java
@@ -1,7 +1,21 @@
 package com.example.hexagonal_rve.domain.user.model;
 
+import com.example.hexagonal_rve.adapter.post.out.db.PostEntity;
+import com.example.hexagonal_rve.adapter.user.out.db.UserEntity;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class User {
   private int id;
   private String accountId;
   private String password;
+
+  public static User restore(UserEntity userEntity){
+    return User.builder()
+        .accountId(userEntity.getAccountId())
+        .password(userEntity.getPassword())
+        .build();
+  }
 }
