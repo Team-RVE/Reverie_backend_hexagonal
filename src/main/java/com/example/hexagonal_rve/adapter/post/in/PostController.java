965611b42f57aba5f1f99825e0034053eed8d75f
yellowstarr0323 <ihansaem60@gmail.com>
2025-12-23 08:25:18 +0900

    postController에 posts를 post로 변경

--- src/main/java/com/example/hexagonal_rve/adapter/post/in/PostController.java
+++ src/main/java/com/example/hexagonal_rve/adapter/post/in/PostController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/posts")
+@RequestMapping("/post")
 public class PostController {
 
   private final CreatePostUseCase createPostUseCase;
